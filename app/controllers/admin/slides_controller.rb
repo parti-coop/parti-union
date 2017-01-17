c8b743bbb26e87ef944141091d8196deeccee103
qus <qus1225@gmail.com>
2017-01-17 16:39:07 +0900

    관리자 아이디와 패스워드를 입력하면 슬라이드관리화면으로 진입할 수 있습니다

---
+++ app/controllers/admin/slides_controller.rb
@@ -0,0 +1,4 @@
+module Admin
+  class SlidesController < BaseController
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get 'campaigns/:slug', to: 'pages#campaign_detail', as: :campaign_detail
   get 'platforms/:slug', to: 'pages#platform_detail', as: :platform_detail
+
+  namespace :admin do
+    resources :slides
+  end
 end
