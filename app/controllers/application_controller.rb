3bf8bf0705ff27af8434905d80128f2ea3d6d6fc
qus <qus1225@gmail.com>
2017-03-21 15:51:06 +0900

    Setting self.response_body = nil no longer prevents DoubleRenderError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :prepare_meta_tags, if: "request.get?"
 
+  if Rails.env.production? or Rails.env.staging?
+    rescue_from ActiveRecord::RecordNotFound, ActionController::UnknownFormat do |exception|
+      render_404
+    end
+
+    rescue_from ActionController::InvalidCrossOriginRequest, ActionController::InvalidAuthenticityToken do |exception|
+      begin
+        self.response_body = nil
+        redirect_to root_url, :alert => I18n.t('errors.messages.invalid_auth_token')
+      rescue AbstractController::DoubleRenderError => e
+      end
+    end
+  end
+
+  def render_404
+    begin
+      self.response_body = nil
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    rescue AbstractController::DoubleRenderError => e
+    end
+  end
+
   def errors_to_flash(model)
     flash[:notice] = model.errors.full_messages.join('<br>').html_safe
   end
