779ffd8d8ef539b12115c770cf0caf2ad75f5f55
Greenhair <martionlee@gmail.com>
2018-09-04 12:01:30 +0900

    홈페이지 영어 버젼을 테스트합니다

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module PartiUnion
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Asia/Seoul'
+    config.i18n.available_locales = [:en, :ko]
+    config.i18n.default_locale = :ko
   end
 end
