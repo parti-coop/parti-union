aef1ef3465ae8e8933c35e6a76a921128bdd729b
Greenhair <martionlee@gmail.com>
2018-09-26 18:40:32 +0900

    플랫폼 소개를 추가하고 슬라이드로 넘길 수 있게 합니다

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require masonry.pkgd
 //= require bootstrap
 //= require blocs.min
+//= require owl.carousel
 
 $(function(){
   $('.masonry-container').masonry();
@@ -10,4 +11,14 @@ $(function(){
 
 $(function(){
   $('.preload-modal').modal('show');
+});
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      navication: true,
+      center: true,
+      loop: false,
+      margin: 30,
+      autoWidth: true
+  })
 });
